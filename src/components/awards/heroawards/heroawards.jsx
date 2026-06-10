fe4659ac9f0ca2e13d2aac8582e5a87f35155202
Sandeep <sandeep.mamloda@vselindia.com>
2026-06-10 16:47:47 +0530

    changes

---
+++ src/components/awards/heroawards/heroawards.jsx
@@ -0,0 +1,9 @@
+import styles from "./heroawards.module.css";
+const Heroawards=()=>{
+    return (
+        <section className={styles["heroawards-main"]}>
+          
+        </section>
+    )
+}
+export default Heroawards;
