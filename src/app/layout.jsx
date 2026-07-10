a8fd95c9cddd4d75b3bec8c7a5573b478fbb20d0
Sandeep <sandeep.mamloda@vselindia.com>
2026-07-10 09:58:59 +0530

    changes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,18 +1,42 @@
+// // import "./globals.css";
+
+// // export default function RootLayout({ children }) {
+// //   return (
+// //     <html lang="en">
+// //       <body>
+// //           {/* <Navbar /> */}
+// //           {children}
+// //           {/* <Footer /> */}
+// //       </body>
+// //     </html>
+// //   );
+// // }
+
+// // --------------------------------------------------------------------------------------
+// "use client";
+
 // import "./globals.css";
+// import { usePathname } from "next/navigation";
+// import Navbar from "@/components/navbar/navbar";
+// import Loader from "@/components/loader/loader";
+// import Footer from "@/components/footer/footer";
 
 // export default function RootLayout({ children }) {
+//   const pathname = usePathname();
+
 //   return (
 //     <html lang="en">
-//       <body>
-//           {/* <Navbar /> */}
-//           {children}
-//           {/* <Footer /> */}
+//       <body style={{backgroundColor:"rgba(255, 249, 255, 1)"}}>
+//         <Loader />
+//         <Navbar/>
+//         {children}
+//         {pathname !== "/" && <Footer />}
 //       </body>
 //     </html>
 //   );
 // }
 
-// --------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
 "use client";
 
 import "./globals.css";
@@ -20,15 +44,17 @@ import { usePathname } from "next/navigation";
 import Navbar from "@/components/navbar/navbar";
 import Loader from "@/components/loader/loader";
 import Footer from "@/components/footer/footer";
+import Saturnbackground from "@/components/satturn/satturnbackground";
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
 
   return (
     <html lang="en">
-      <body style={{backgroundColor:"rgba(255, 249, 255, 1)"}}>
+      <body style={{ backgroundColor: "rgba(255, 249, 255, 1)" }}>
+        <Saturnbackground />
         <Loader />
-        <Navbar/>
+        <Navbar />
         {children}
         {pathname !== "/" && <Footer />}
       </body>
